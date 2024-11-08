d87f3f748756c322661b1083672cabe6a337473b
ruan <ruanlazareti@hotmail.com>
2024-11-08 15:05:29 -0300

    added coins in player model

--- src/app/models/player.model.ts
+++ src/app/models/player.model.ts
@@ -3,9 +3,11 @@ import {Card} from "./card.model";
 export class Player {
   name: string;
   session_id: string;
+  coins: number;
 
-  constructor(name: string, session_id: string) {
+  constructor(name: string, session_id: string, coins: number) {
     this.name = name;
     this.session_id = session_id;
+    this.coins = coins;
   }
 }
