c925049c6d7640ab3196f60e43359daa7d0c4a69
Ruan Lazareti <111817497+Ruan-Lazareti@users.noreply.github.com>
2025-02-27 11:45:02 -0300

    feat: broadcast player join event and update player list
    - Emit new player event via Pusher
    - Update player list in real-time for all connected clients

--- src/app/game/join-game/join-game.component.ts
+++ src/app/game/join-game/join-game.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import Echo from "laravel-echo";
 
 import { GameService } from "./game.service";
 import { Player } from "../../player/player.model"
-import {start} from "@popperjs/core";
 import {Game} from "../game.model";
 
 @Component({
@@ -14,22 +14,21 @@ import {Game} from "../game.model";
   standalone: true,
   imports: [
     FormsModule,
-      CommonModule,
+    CommonModule,
   ],
   styleUrl: './join-game.component.css'
 })
 
 export class JoinGameComponent implements OnInit {
   gameId: string | null = '';
-  nickname: string = '';
   players: Player[] = [];
-  gameStatus: string = '';
+  private echo!: Echo<any>;
 
   constructor(
-      private gameService: GameService,
-      private route: ActivatedRoute,
-      private router: Router,
-  ) {}
+    private gameService: GameService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
     this.gameId = this.route.snapshot.paramMap.get('game_id');
@@ -38,27 +37,22 @@ export class JoinGameComponent implements OnInit {
 
     if (this.gameId) {
       this.gameService.listenToPlayers(this.gameId, (newPlayer: Player) => {
-        this.players.push(newPlayer);
+        const exists = this.players.some(player => player.nickname === newPlayer.nickname);
+
+        if (!exists) {
+          this.players.push(newPlayer);
+        }
       });
     }
   }
 
   getGame() {
-    this.gameService.getGame(this.gameId).subscribe((resp: Game):void => {
-      this.players = resp.players.map((nickname:string) => ({ nickname }))
-      console.log(this.players)
+    this.gameService.getGame(this.gameId).subscribe((resp: Game): void => {
+      this.players = resp.players.map((nickname: string) => ({ nickname }))
     });
   }
-
- /* addPlayer() {
-    this.gameService.addPlayer(this.nickname, this.gameId).subscribe((newPlayer: Player) => {
-      this.nickname = '';
-    });
-  }*/
 
   startGame() {
     this.gameService.startGame(this.gameId).subscribe(() => {})
   }
-
-  protected readonly start = start;
 }
